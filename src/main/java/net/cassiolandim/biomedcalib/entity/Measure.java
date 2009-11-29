2a7af9162909380e9c19aaae477d21391731ecbf
Cassio Landim <ziglee@gmail.com>
2009-11-29 18:44:29 -0200

    Added new operations to MeasuresAggregate

---
+++ src/main/java/net/cassiolandim/biomedcalib/entity/Measure.java
@@ -0,0 +1,9 @@
+package net.cassiolandim.biomedcalib.entity;
+
+/**
+ * @author Cassio Landim
+ */
+public class Measure {
+
+	public Long value;
+}
--- src/main/java/net/cassiolandim/biomedcalib/entity/MeasuresAggregate.java
+++ src/main/java/net/cassiolandim/biomedcalib/entity/MeasuresAggregate.java
@@ -1,8 +1,48 @@
 package net.cassiolandim.biomedcalib.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import net.cassiolandim.biomedcalib.math.CoeffientOfVariation;
+
+import org.apache.commons.math.stat.descriptive.moment.Mean;
+import org.apache.commons.math.stat.descriptive.moment.StandardDeviation;
+
 /**
  * @author Cassio Landim
  */
 public class MeasuresAggregate {
 
+	private List<Measure> measures;
+	
+	public MeasuresAggregate() {
+		measures = new ArrayList<Measure>();
+	}
+	
+	public void addMeasure(Measure measure) {
+		measures.add(measure);
+	}
+
+	public double getMean() {
+		Mean mean = new Mean();
+		
+		for(Measure measure : measures)
+			mean.increment(measure.value);
+		
+		return mean.getResult();
+	}
+	
+	public double getStandardDeviation(){
+		StandardDeviation sd = new StandardDeviation();
+		
+		for(Measure measure : measures)
+			sd.increment(measure.value);
+		
+		return sd.getResult();
+	}
+
+	public double getCofficientOfVariation() {
+		CoeffientOfVariation cv = new CoeffientOfVariation(getMean(), getStandardDeviation());
+		return cv.getResult();
+	}
 }
